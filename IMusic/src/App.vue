8e5480171c0c9b0bd791945acf59414f43384e69
linxinpeng <lxp@lantushiji.com>
2018-12-25 17:07:23 +0800

    新建项目

---
+++ IMusic/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="lt-box">
+      <p>{{msg}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .lt-box{
+    p{
+      font-size: 26px;
+    }
+  }
+</style>
