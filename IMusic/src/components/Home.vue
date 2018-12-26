9809eebd1890e5c5104b28c7706a0249883073ea
linxinpeng <lxp@lantushiji.com>
2018-12-26 15:39:46 +0800

    首页

--- IMusic/src/components/Home.vue
+++ IMusic/src/components/Home.vue
@@ -29,7 +29,31 @@
         </div>
         <div class="h-recom">
             <h3>推荐歌单<i class="ion ion-ios-arrow-forward" /></h3>
+            <ul>
+                <li v-for="(item,index) in recom" :key="index">
+                    <div class="r-img">
+                        <img :src="item.src" />
+                        <i class="ion ion-ios-play-circle"></i>
+                    </div>
+                    <div class="r-footer">
+                        <h4>{{item.title}}</h4>
+                        <p>{{item.singer}}</p>
+                    </div>
+                </li>
+            </ul>
         </div>
+        <div class="h-play">
+            <div class="p-inner">
+                <div class="p-progress" :style="`width:${value}%`"></div>
+                <span class="i-left"><img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1752432182,2397597091&fm=26&gp=0.jpg" /></span>
+                <span class="i-center">
+                    <h4>朗朗的新专辑</h4>
+                    <p>朗朗</p>
+                </span>
+                <span class="i-right" @click="onPlay"><i class="far fa-play-circle"></i></span>
+            </div>
+        </div>
+        <div style="height: 62px"></div>
     </div>
 </template>
 <script>
@@ -38,7 +62,8 @@ export default {
         return{
             hrefs:[
                 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=342462208,241731034&fm=26&gp=0.jpg',
-                'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1751299247,2746970245&fm=26&gp=0.jpg'
+                'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1751299247,2746970245&fm=26&gp=0.jpg',
+                'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2833203770,4110281004&fm=26&gp=0.jpg'
             ],
             list:[
                 { icon: 'ion ion-md-list-box',title:'歌单',color:'rgba(37,214,216,.9)' },
@@ -49,7 +74,36 @@ export default {
                 { icon: 'ion ion-md-browsers',title:'日推' ,color:'rgba(244,76,177,.9)' },
                 { icon: 'ion ion-ios-headset',title:'歌手',color:'rgba(139,104,200,.9)'  },
                 { icon: 'ion ion-md-tv',title:'MV',color:'rgba(37,214,216,.9)'  }
-            ]
+            ],
+            recom:[
+                {
+                    src:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=342462208,241731034&fm=26&gp=0.jpg',
+                    title:'一入电音深似海',
+                    singer:'Dearrn'
+                },
+                 {
+                    src:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1751299247,2746970245&fm=26&gp=0.jpg',
+                    title:'最好听的还是你',
+                    singer:'Mr.Zhou'
+                },
+                 {
+                    src:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2833203770,4110281004&fm=26&gp=0.jpg',
+                    title:'懂我的，不懂我的',
+                    singer:'Ruerer'
+                }
+            ],
+            value:0,
+            time: null
+        }
+    },
+    methods:{
+        onPlay(){
+          this.time = setInterval(()=>{
+               this.value++
+               if(this.value == 100){
+                   clearInterval(this.time)
+               }
+           },100)
         }
     }
 }
@@ -64,7 +118,7 @@ export default {
             }
         }
         .h-banner{
-            height: 200px;
+            height: 130px;
             width: calc(100% - 20px);
             margin: 10px auto;
             border-radius: 5px;
@@ -115,6 +169,100 @@ export default {
                     float: right;
                     font-size: 17px;
                     margin-top: 3px;
+                }
+            }
+            ul{
+                li{
+                    display: inline-block;
+                    width: calc( 100% / 3 - 4px);
+                    margin: 2px;
+                    .r-img{
+                        width: 100%;
+                        position: relative;
+                        img{
+                            max-width: 100%;
+                            max-height: 100%;
+                            border-radius: 5px;
+                            box-shadow: 0 0 10px;
+                        }
+                        i{
+                            position: absolute;
+                            bottom: 10px;
+                            right: 10px;
+                        }
+                    }
+                    .r-footer{
+                        h4{
+                            margin: 5px auto;
+                            font-size: 13px;
+                            font-weight: 400;
+                            overflow: hidden;
+                        }
+                        p{
+                            margin: 0;
+                            font-size: 12px;
+                            color: #aaa;
+                        }
+                    }
+                }
+            }
+           
+        }
+        .h-play{
+            position: fixed;
+            bottom: 0;
+            background: #fff;
+            height: 61px;
+            width: 100%;
+            .p-inner{
+                height: 60px;
+                position: relative;
+                border-top: 1px solid #eee;
+                .p-progress{
+                    position: absolute;
+                    bottom: 0;
+                    height: 1px;
+                    z-index: 2;
+                    background: red;
+                    transition: all .3s ease;
+                }
+                 span{
+                    float: left;
+                }
+                .i-left{
+                    width: 10%;
+                    margin-left: 20px;
+                    padding-top: 10px;
+                    box-sizing: border-box;
+                    img{
+                        max-width: 100%;
+                        max-height: 100%;
+                    }
+                }
+                .i-center{
+                    width: 60%;
+                    padding-left: 15px;
+                    padding-top: 10px;
+                    box-sizing: border-box;
+                    h4{
+                        margin: 0;
+                        font-size: 12px;
+                        // margin-top: 4px;
+                        color: #666;
+                    }
+                    p{
+                        margin: 0;
+                        font-size: 12px;
+                        margin-top: 3px;
+                    }
+                }
+                .i-right{
+                    padding-top: 13px;
+                    box-sizing: border-box;
+                    padding-left: 24px;
+                    width: calc(30% - 20px);
+                    color: rgba(37, 214, 216, 0.9);
+                    font-size: 28px;
                 }
             }
            
